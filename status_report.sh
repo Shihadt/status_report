eb92526c83d66c97be8cea4fcb91d7ba74ce2255
shihadt <shihad@qburst.com>
2018-08-23 11:29:37 +0530

    Corrected installation and configuration

--- status_report.sh
+++ status_report.sh
@@ -5,20 +5,22 @@ echo "Starting status report api service....."
 if [ ! -f "$( echo $paths)/conf.in" ]; then
 
     echo "Installing necessary libraries"
-    https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+    wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
     sudo unzip -o chromedriver_linux64.zip -d /usr/bin/
 
     pip install --upgrade google-api-python-client oauth2client
     pip install httplib2
+    pip install selenium
 
 	echo "Configuring mail service."
     echo "Enable Gmail api https://developers.google.com/gmail/api/quickstart/python. And Download the secret key."
-    echo "Press any key After registering google gmail API"
+    echo "Press any key After registering google gmail API."
     read temp
     echo "Moving secret ket to installation location"
     cp ~/Downloads/credentials.json $paths
-    echo "Renaming credential.json to client_secret.json"
-    mv $(echo $paths)/credentials.json client_secret.json
+    echo "Configuring api compose."
+    python setup.py $paths
+
 	echo "Enter your email id : "
 	read myid
 	echo "Enter TO email id : "
@@ -33,13 +35,13 @@ if [ ! -f "$( echo $paths)/conf.in" ]; then
     read your_name
     echo "Enter your phone number: "
     read your_phone
-    sed -e "s/Your\ name/$(echo $your_name)/1" body.html
-    sed -e "s/0000000000/$(echo $your_phone)/1" body.html
+    sed -e "s/Your\ Name/$(echo $your_name)/1" body.html
+    sed -e "s/Your\ Phone/$(echo $your_phone)/1" body.html
 fi
 echo "Enter work done"
-xdg-open $(echo $paths)/work_done.txt
+vim $(echo $paths)/work_done.txt
 echo "Enter to do list"
-xdg-open $(echo $paths)/todo.txt
+vim $(echo $paths)/todo.txt
 echo "Created todo list and workdone list.\n Generating mail using google API"
 python $(echo $paths)/index.py $paths
 echo "succesfully created and send mail."
